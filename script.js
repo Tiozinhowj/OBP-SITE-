7bd9da4a6354a641d4ebdece82449c8e1900f6b2
William Pereira Ramos <william.pramos@outlook.com>
2024-03-11 17:20:54 -0300

    11

--- script.js
+++ script.js
@@ -2,3 +2,16 @@ function AbrirChamado() {
     // Duplicando as barras invertidas
     location.href = "./Abrir Chamado/login.html";
 }
+
+const btlogin = document.querySelector('.btlogin')
+const menuicon = document.querySelector('.menuicon i')
+const navigationMENU = document.querySelector('.navigationMENU')
+
+menuicon.onclick = function () {
+    navigationMENU.classList.toggle('open')
+    const isOpen = navigationMENU.classList.contains('open')
+
+    menuicon.classList = isOpen
+    ? 'bx bx-menu'
+    : 'bx bx-x-circle'
+}
