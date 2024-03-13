d2fc31db09d07a6d053013a784f89aaa9ce3f374
William Pereira Ramos <william.pramos@outlook.com>
2024-03-13 17:43:16 -0300

    14

--- Produtos/produtos.js
+++ Produtos/produtos.js
@@ -4,3 +4,11 @@ function AbrirChamado() {
 }
 
 
+window.sr = ScrollReveal({ reset: true});
+
+sr.reveal('.box1' ,{duration: 2500})
+sr.reveal('.indeximg1' ,{duration: 6000})
+sr.reveal('.box2' ,{duration: 6000})
+sr.reveal('.indeximg2' ,{duration: 3000})
+sr.reveal('.box3' ,{duration: 3000})
+sr.reveal('indeximg3' ,{duration: 6000})
