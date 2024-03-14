44d99a5e335910da403dd43c08ff55f283ed246b
William Pereira Ramos <william.pramos@outlook.com>
2024-03-14 12:15:37 -0300

    15

--- Produtos/produtos.js
+++ Produtos/produtos.js
@@ -3,11 +3,23 @@ function AbrirChamado() {
     location.href = "../Abrir Chamado/login.html";
 }
 
+const btlogin = document.querySelector('.btlogin')
+const menuicon = document.querySelector('.menuicon i')
+const navigationMENU = document.querySelector('.navigationMENU')
+
+menuicon.onclick = function () {
+    navigationMENU.classList.toggle('open')
+    const isOpen = navigationMENU.classList.contains('open')
+
+    menuicon.className = isOpen
+        ? 'bx bx-x-circle'
+        : 'bx bx-menu';
+}
 
 window.sr = ScrollReveal({ reset: true});
 
 sr.reveal('.box1' ,{duration: 2500})
-sr.reveal('.indeximg1' ,{duration: 6000})
+sr.reveal('.indeximg1' ,{duration: 6000} )
 sr.reveal('.box2' ,{duration: 6000})
 sr.reveal('.indeximg2' ,{duration: 3000})
 sr.reveal('.box3' ,{duration: 3000})
